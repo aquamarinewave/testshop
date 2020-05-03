f95a93de6f196a606c514c91582314ef752f9d9e
aquamarinewave <50908486+aquamarinewave@users.noreply.github.com>
2020-05-04 01:49:17 +0300

    Update mail.php

--- php_mail/mail.php
+++ php_mail/mail.php
@@ -1,14 +1,23 @@
 <?php
+$data = file_get_contents('php://input');
+var_dump(data);
 
 //Message
-$message = "New buy";
+//$message = "New buy";
 
 //Sent mail 
-$mail = mail('medynska.maryna@gmail.com', 'MyGoogle Shop, $message');
-if ($mail) {
-    echo 'yes';
-} else {
-    echo 'no';
-}
+// $mail = mail('medynska.maryna@gmail.com', 'MyGoogle Shop, $message');
+// if ($mail) {
+//     echo 'yes';
+// } else {
+//     echo 'no';
+// }
+
+
+
+
+
+
+
 
 ?>
